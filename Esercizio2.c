88cf84ce1c21c59823f75d9c801afb0d673a15fc
sebrs3018 <sebastian.r.s3107@hotmail.com>
2021-04-09 15:05:16 +0200

    MatrixUtils Done

--- Esercizio2.c
+++ Esercizio2.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 
 static volatile int NUM_TREADS = 4;
 
-static volatile int A_rows = 4;
-static volatile int A_cols = 4;
-static volatile int B_rows = 4;
-static volatile int B_cols = 4;
+static volatile int A_rows = 3;
+static volatile int A_cols = 3;
+static volatile int B_rows = 3;
+static volatile int B_cols = 3;
 static volatile int C_rows = 4;
 static volatile int C_cols = 4;
 
 
-//axb  bxc
-struct args {
+
+
+typedef struct args {
     float** LeftMatrix;
     float** RightMatrix;
     int LeftCols, RightCols;
     int whichRow;
-    int blockLength;
-};
+    int howManyRows;
+    int from, to;
+} Args;
+
+
+
+
+Args Initialize_Args(float** LeftMatrix, float** RightMatrix,
+         int leftCols, int rightCols, int from, int to){
+  
+   return (Args) {.LeftMatrix = LeftMatrix, .RightMatrix = RightMatrix, 
+   .LeftCols = leftCols, .RightCols = rightCols, .from = from, .to = to};
+}
+
 
 // pthread_barrier_t   barrier;
 //La dimensione della matrice risultante sarà uguale a: m ed y
@@ -64,33 +78,36 @@ void *RowColMultiplication(void *input)
 
     int Row_Dim = ((struct args*)input)->LeftCols; //dimensione della riga => LeftCols
     int cols = ((struct args*)input)->RightCols;
-    int row = ((struct args*)input)->whichRow;
-    int blockLength = ((struct args*)input)->blockLength;
+    // int row = ((struct args*)input)->whichRow;
+    // int blockLength = ((struct args*)input)->howManyRows; //numero di righe per blocco
 
-    // printf("thread_no: %d\n", row);
+    int from = ((struct args*)input)->from;
+    int to = ((struct args*)input)->to;
 
+    // printf("from: %d to:%d\n", from, to);
     float buffer = 0;
 
 
-    // for (int m = 0; m < blockLength; m++)
-    // {
+    for (int m = from; m < to; m++)
+    {
+      //per ogni colonna della RightMatrix
       for (int i = 0; i < cols; i++)
       {
-        
+        //per ogni elem della riga LeftMatrix, la moltiplico con la colonna i-esima di RightMatrix
         for (int j = 0; j < Row_Dim; j++)
         {
             // printf("%f * %f =", ((struct args*)input)->LeftMatrix[row][j], ((struct args*)input)->RightMatrix[j][i]);
 
-            buffer = buffer + ((struct args*)input)->LeftMatrix[row][j] * ((struct args*)input)->RightMatrix[j][i] ;
+            buffer = buffer + ((struct args*)input)->LeftMatrix[m][j] * ((struct args*)input)->RightMatrix[j][i] ;
             //  printf("%f\t", buffer);
         }
 
-        result[row][i] = buffer;
+        result[m][i] = buffer;
         // printf("row:%d col:%d value: %f" , row, i, result[row][i]);
         buffer = 0;
         //  printf("\n");
       }
-    // }
+    }
 
 }
 
@@ -165,7 +182,7 @@ int main(int argc, char *argv[])
 
     if(argc != 5) {
         printf("Invalid arguments, matrixes will have default sizes:\n");
-        printf("\t- A(4x4)\n\t- B(4x4)\n\t- C(4x4)\n");
+        printf("\t- A(%dx%d)\n\t- B(%dx%d)\n\t- C(%dx%d)\n", A_rows, A_cols, B_rows, B_cols, C_rows, C_cols);
     }
     else{
         A_rows = atoi(argv[1]);
@@ -180,7 +197,7 @@ int main(int argc, char *argv[])
     }
 
 
-    pthread_t my_threads[NUM_TREADS];
+    pthread_t my_threads[2];
     void * returnCode;
 
     int Tresult;
@@ -199,34 +216,67 @@ int main(int argc, char *argv[])
     prnt_matrix(B, B_rows, B_cols);
     printf("\n");
 
+    bool isMultiple = true;
+    int howManyBlocks = 2; //voglio 2 blocchi
+    int offset = 0;
 
+    if((offset = A_rows % howManyBlocks) != 0)
+        isMultiple = false;
+    
+    int BlockLength = A_rows / howManyBlocks; //dimensione dei blocchi
+    int thread_no = 0;
+    int from = 0, to = 0;
+    //in questo caso si lavora con 4 threads
 
 
     /* Start the threads */
     for (int i = 0; i < A_rows; i++)
     {
-      /* Dichiaro struct di elementi */
-        struct args *Matrixes = (struct args*)malloc(sizeof(struct args));
-        Matrixes->LeftMatrix  = A;
-        Matrixes->RightMatrix = B;
-        Matrixes->RightCols = B_cols;
-        Matrixes->whichRow = i;
-        Matrixes->LeftCols = A_cols;
 
+      if(!isMultiple && i + 1 == offset ){
+        /* Dichiaro struct di elementi */
+        struct args *Matrixes = (struct args*)malloc(sizeof(struct args));
+        *Matrixes = Initialize_Args(A,B, A_cols, B_cols, i+1 - offset, i+1);
+        // Matrixes->LeftMatrix  = A;
+        // Matrixes->RightMatrix = B;
+        // Matrixes->RightCols = B_cols;
+        // Matrixes->LeftCols = A_cols;
+
+
+        // Matrixes->to   = i+1;
+        // Matrixes->from = i+1 - offset; 
         // printf("%d\t", Matrixes->whichRow);
-        Tresult = pthread_create(&my_threads[i], NULL, &RowColMultiplication, (void*)Matrixes);
+        Tresult = pthread_create(&my_threads[thread_no++], NULL, &RowColMultiplication, (void*)Matrixes);
+        isMultiple = true;
+      }
+      else if((i+1)%BlockLength == 0 && isMultiple){
+        /* Dichiaro struct di elementi */
+        struct args *Matrixes = (struct args*)malloc(sizeof(struct args));
+        *Matrixes = Initialize_Args(A,B, A_cols, B_cols, i+1 - BlockLength, i+1);
+        // Matrixes->LeftMatrix  = A;
+        // Matrixes->RightMatrix = B;
+        // Matrixes->RightCols = B_cols;
+        // Matrixes->LeftCols = A_cols;
+
+        // Matrixes->to   = i+1;
+        // Matrixes->from = i+1 - BlockLength; 
+        // printf("to: %d  from: %d\t", Matrixes->from, Matrixes->to );
+        Tresult = pthread_create(&my_threads[thread_no++], NULL, &RowColMultiplication, (void*)Matrixes);
+      }
+
+
     }
 
 
     // pthread_barrier_wait (&barrier);
 
     /* Wait for the threads to end */
-    for (int i = 0; i < A_rows; i++)
+    for (int i = 0; i < thread_no; i++)
     {
        Tresult = pthread_join(my_threads[i], &returnCode);
     }
 
     //Da notare che la dimensione della matrice risultate è nota!
-    printf("All threads terminated\n");
+    printf("All %d threads terminated\n", thread_no);
     prnt_matrix(result, A_rows, B_cols);
 }
