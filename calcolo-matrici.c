bd12a5de3e63311232510c061a531248b3d8882e
iliass-moualy <iliassmoualy99@gmail.com>
2021-04-10 13:40:43 +0200

    versione iliass

--- calcolo-matrici.c
+++ calcolo-matrici.c
@@ -1,98 +1,250 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <pthread.h>
+#include <stdbool.h>
 
-static volatile int A_rows = 4;
-static volatile int A_cols = 4;
-static volatile int B_rows = 4;
-static volatile int B_cols = 4;
+
+static volatile int NUM_THREADS = 4;
+
+
+static volatile int A_rows = 3;
+static volatile int A_cols = 3;
+static volatile int B_rows = 3;
+static volatile int B_cols = 3;
 static volatile int C_rows = 4;
 static volatile int C_cols = 4;
 
 
-void fill_matrix(float **matrix, int rows, int cols) {
 
-    int i, j;
-    float a = 5.0;
 
-   
-    for (i=0; i<rows; i++){
-        matrix[i] = malloc(cols * sizeof *matrix[i]);
-    }
+typedef struct args {
+    float** LeftMatrix;
+    float** RightMatrix;
+    int LeftCols, RightCols;
+    int from, to;
+} Args;
 
-    for (i = 0; i < rows; i++)
+
+
+Args Initialize_Args(float** LeftMatrix, float** RightMatrix,
+         int leftCols, int rightCols, int from, int to){
+  
+   return (Args) {.LeftMatrix = LeftMatrix, .RightMatrix = RightMatrix, 
+   .LeftCols = leftCols, .RightCols = rightCols, .from = from, .to = to};
+}
+
+
+
+// pthread_barrier_t   barrier;
+//La dimensione della matrice risultante sarà uguale a: m ed y
+float **result;
+
+//Precondition:
+/*
+    TestCases:
+        LeftMatrix[3][3] && RightMatrix[3][1] --> testando...
+        LeftMatrix[3][3] && RightMatrix[3][8]
+        LeftMatrix[1][3] && RightMatrix[3][8]
+*/
+//Questi vettori, per vincolo moltiplicazione, hanno la stessa lunghezza!
+
+void *RowColMultiplication(void *input)
+{
+    //  printf("entering..");
+
+    int Row_Dim = ((struct args*)input)->LeftCols; //dimensione della riga => LeftCols
+    int cols = ((struct args*)input)->RightCols;
+    // int row = ((struct args*)input)->whichRow;
+    // int blockLength = ((struct args*)input)->howManyRows; //numero di righe per blocco
+
+    int from = ((struct args*)input)->from;
+    int to = ((struct args*)input)->to;
+
+    printf("from: %d to:%d\n", from, to);
+    float buffer = 0;
+
+
+    for (int m = from; m < to; m++)
     {
-        for (j = 0; j < cols; j++)
+      //per ogni colonna della RightMatrix
+      for (int i = 0; i < cols; i++)
+      {
+        //per ogni elem della riga LeftMatrix, la moltiplico con la colonna i-esima di RightMatrix
+        for (int j = 0; j < Row_Dim; j++)
         {
-            matrix[i][j] = ((float)rand()/(float)(RAND_MAX)) * a;
+            // printf("%f * %f =", ((struct args*)input)->LeftMatrix[row][j], ((struct args*)input)->RightMatrix[j][i]);
+
+            buffer = buffer + ((struct args*)input)->LeftMatrix[m][j] * ((struct args*)input)->RightMatrix[j][i] ;
+            //  printf("%f\t", buffer);
         }
+
+        result[m][i] = buffer;
+        // printf("row:%d col:%d value: %f" , row, i, result[row][i]);
+        buffer = 0;
+        //  printf("\n");
+      }
     }
 
 }
 
-void print_matrix(float **matrix, int rows, int cols) {
-    int i = 0;
-    int j = 0;
 
-    for(i = 0 ; i < rows ; i++) {
-        printf(" (");
-        for(j = 0 ; j < cols ; j++){
-            printf("%f     ", matrix[i][j]);
+void prnt_matrix(float ** matrix,int m,int n)
+{
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%f ",matrix[i][j]);
         }
-        printf(")\n");
+        printf("\n");
     }
 }
 
+float ** create_matrix(int m, int n, float starter)
+{
+  float ** ma = calloc(m, sizeof( float*));
 
+  if (ma != NULL) {
+    int i, j;
+
+    for (i = 0; i != m; ++i) {
+      if ((ma[i] = malloc(n*sizeof(float))) == NULL)
+        return NULL;
+      for (j = 0; j != n; ++j) {
+        ma[i][j] = i*n + j + starter;
+      }
+    }
+
+  }
+  return ma;
+}
+
+
+void obtn_matrix(float *** matrix, float * m, float * n)
+{
+    printf("Please enter the row number: ");
+    fflush(stdin);
+    scanf("%f",m);
+
+    printf("Please enter the column number: ");
+    fflush(stdin);
+    scanf("%f",n);
+
+    printf("Please enter starter number: ");
+    fflush(stdin);
+
+    *matrix=create_matrix(*m,*n, 0);
+}
+
+void free_matrix(float ** matrix, int m, int n)
+{
+    for(int i=0;i<m;i++)
+    {
+      if (matrix[i] == NULL)
+        /* matrix creation was aborted */
+        break;
+      free(matrix[i]);
+    }
+
+    free(matrix);
+}
 
 int main(int argc, char *argv[])
 {
-    if(argc != 5) {
-        printf("Invalid arguments, matrixes will have default sizes:\n");
-        printf("\t- A(4x4)\n\t- B(4x4)\n\t- C(4x4)\n");
+  int i = 0;
+
+  if(argc != 5) {
+      printf("Invalid arguments, matrixes will have default sizes:\n");
+      printf("\t- A(%dx%d)\n\t- B(%dx%d)\n\t- C(%dx%d)\n", A_rows, A_cols, B_rows, B_cols, C_rows, C_cols);
+  }
+  else{
+      A_rows = atoi(argv[1]);
+      A_cols = atoi(argv[2]);
+      B_rows = atoi(argv[2]);
+      B_cols = atoi(argv[3]);
+      C_rows = atoi(argv[3]);
+      C_cols = atoi(argv[4]);
+
+      printf("Matrixes sizes:\n");
+      printf("\t- A(%dx%d)\n\t- B(%dx%d)\n\t- C(%dx%d)\n", A_rows, A_cols, B_rows, B_cols, C_rows, C_cols);
+  }
+
+/***************************
+   **  DECOMPOSIZIONE DI A  ** 
+   ***************************/
+  // se righe di A sono multiplo del numero massimo di blocchi allora block size sarà:
+  //    A_rows/MAX_BLOCKS
+  // altrimenti:
+  //    prendiamo A' che è la più grande sottomatrice di A t.c. abbia numero di righe multiplo di MAX_BLOCKS
+  //    determiniamo block_size tramite = A'_rows/MAX_BLOCKS
+  //    a questo punto le k righe lasciate fuori saranno di numero al massimo: MAX_BLOCK - 1
+  //    queste k righe rimanenti le assegniamo ai primi k thread
+  //
+  //in questo modo decomponiamo uniformemente nel caso la matrice avesse una size multipla del numero di blocchi massimo supportato
+  //ed avremmo una decomposizione quasi uniforme nel caso di size non multipla
+  
+
+  int blocks_per_thread = 1;
+
+  const int MAX_BLOCKS = blocks_per_thread * NUM_THREADS;
+  int blocks_number = 0;
+  int blocks_size = 0;
+  int rows_left = 0;
+  int is_multiple = true;
+  
+  if(A_rows <= MAX_BLOCKS){
+    blocks_number = A_rows;
+    blocks_size = 1;
+  }
+  else {
+    if(A_rows % MAX_BLOCKS == 0){
+      blocks_size = A_rows / MAX_BLOCKS;
+      blocks_number = MAX_BLOCKS; 
     }
     else{
-        A_rows = atoi(argv[1]);
-        A_cols = atoi(argv[2]);
-        B_rows = atoi(argv[2]);
-        B_cols = atoi(argv[3]);
-        C_rows = atoi(argv[3]);
-        C_cols = atoi(argv[4]);
+      rows_left = A_rows % MAX_BLOCKS;
+      blocks_size = (A_rows - rows_left) / MAX_BLOCKS;
+      is_multiple = false;
+      blocks_number = MAX_BLOCKS; 
 
-        printf("Matrixes sizes:\n");
-        printf("\t- A(%dx%d)\n\t- B(%dx%d)\n\t- C(%dx%d)\n", A_rows, A_cols, B_rows, B_cols, C_rows, C_cols);
     }
-    
-    srand((unsigned int)time(NULL));
-    
-    int i;
+  }
 
-    float **A = (float **)malloc(A_rows * A_cols * sizeof(float));
-    for (i=0; i < A_rows; i++)
-         A[i] = (float *)malloc(A_cols * sizeof(float));
+  // assegnazione ai thread 
 
-    float **B = (float **)malloc(B_rows * B_cols * sizeof(float));
-    for (i=0; i < B_rows; i++)
-         B[i] = (float *)malloc(B_cols * sizeof(float));
+  int k = 1;
+  int prev = 1;
+
+  pthread_t my_threads[NUM_THREADS];
+  float **A, **B, **C;
+
+  A = create_matrix( A_rows, A_cols, 1 );
+  B = create_matrix( B_rows, B_cols, 5 );
+  C = create_matrix( C_rows, C_cols, 9 );
+
+  
+  // prnt_matrix(result, A_rows, B_cols);
+  prnt_matrix(A, A_rows, A_cols);
+  printf("\n");
+  prnt_matrix(B, B_rows, B_cols);
+  printf("\n");
 
 
-    float **C = (float **)malloc(C_rows * C_cols * sizeof(float));
-    for (i=0; i < C_rows; i++)
-         C[i] = (float *)malloc(C_cols * sizeof(float));
-    
+  result = create_matrix( A_rows, B_cols, 1 );
 
-    fill_matrix(A, A_rows, A_cols);
-    fill_matrix(B, B_rows, B_cols);
-    fill_matrix(C, C_rows, C_cols);
-    
-    printf("A:\n");
-    print_matrix(A, A_rows, A_cols);
 
-    printf("B:\n");
-    print_matrix(B, B_rows, B_cols);
+  for(i = 0; i < blocks_number; ++i){
+    if(i == rows_left + 1)
+      k = 0;
+    if(i == rows_left + 2)
+      prev = 0;
+    struct args *Matrixes = (struct args*)malloc(sizeof(struct args));
+    *Matrixes = Initialize_Args(A,B, A_cols, B_cols, i * blocks_size , (i+1) * blocks_size - 1 + k);
+    pthread_create(&my_threads[i++], NULL, &RowColMultiplication, (void*)Matrixes);
+  }
 
-    printf("C:\n");
-    print_matrix(C, C_rows, C_cols);
 
-    return 0;
+  prnt_matrix(result, A_rows, B_cols);
+
 }
